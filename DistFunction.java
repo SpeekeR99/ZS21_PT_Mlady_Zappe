9466fb8b6fc7743b98e4cab57cedb7d30738a90b
SpeekeR <DominikZappe1999@seznam.cz>
2021-10-10 17:39:10 +0200

    BugFix in calculating CartesianDist dist

--- DistFunction.java
+++ DistFunction.java
@@ -15,8 +15,8 @@ class CartesianDist implements DistFunction {
 
     @Override
     public double dist(GraphNode a, GraphNode b) throws IllegalArgumentException {
-            double difX = a.x- b.x;
-            double difY = b.x-b.y;
+            double difX = a.x - b.x;
+            double difY = a.y - b.y;
             return Math.sqrt(difX*difX+difY*difY);
     }
 }
