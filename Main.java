2577cc56ff2c2e5d9486cd6e204eab28a815a638
SpeekeR <DominikZappe1999@seznam.cz>
2021-10-28 11:42:46 +0200

    Nejde mi dát pull

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         Parser parser;
         try {
-            parser = new Parser("data/random1000.txt");
+            parser = new Parser("data/grid2000.txt");
         } catch (FileNotFoundException e) {
             System.out.println("Chyba při načítání souboru:");
             e.printStackTrace();
@@ -58,8 +58,8 @@ public class Main {
 
 
         /* vypis testovaci, ze v nodes je vse jak ma byt
-        * to jest - na 0 indexu je paris, hned potom jsou letadla a
-        * az nakonec jsou kone (+ indexy v nodes, takze napr kun1 = index prvniho kone - pocet letadel) */
+         * to jest - na 0 indexu je paris, hned potom jsou letadla a
+         * az nakonec jsou kone (+ indexy v nodes, takze napr kun1 = index prvniho kone - pocet letadel) */
         /*
         for(int i = 0; i < nodes.length; i++) {
             if (nodes[i] instanceof Horse) {
@@ -73,7 +73,6 @@ public class Main {
                 System.out.println(nodes[i].x + " " + nodes[i].y);
             }
         }
-
         int len = 0;
         for (int i = 1; nodes[i] instanceof Aircraft ; i++) {
             len+= getClosestHorses(nodes,numberOfAircrafts+1,(Aircraft) nodes[i]).length;
@@ -141,13 +140,11 @@ public class Main {
 /*
 class GraphTest{
     static Random r = new Random(1);
-
     public static void main(String[] args) {
         int val = 1_051_442_000;
         char[] a = new char[val];
         System.out.println(val);
     }
-
     public static void testHeap(){
         MinHeap h = new MinHeap(10_000);
         for (int i = 0; i < h.size(); i++) {
@@ -157,33 +154,26 @@ class GraphTest{
         for (int i = 0; i < h.size(); i++) {
             p[i] = h.pop().weight;
         }
-
         for (int i = 1; i < p.length; i++) {
             if(p[i-1]>p[i]) {
                 System.out.println("NE "+i);
-
             }
         }
-
         for (double v :p) {
             System.out.println(v);
         }
     }
     public static void testGraph() {
-
         Place[] nodes = new Place[7000];
         for (int i = 0; i < nodes.length; i++) {
             nodes[i] = new Place(100*r.nextDouble(), 100*r.nextDouble());
         }
-
         AdjMatrixGraph<Place> graph = new AdjMatrixGraph<>(nodes, new ClosestNeighbourPath(0));
-
         for (int i = 0; i < nodes.length; i++) {
             for (int j = 0; j < nodes.length; j++) {
                 graph.addEdge(i,j,nodes[i].dist(nodes[j]));
             }
         }
-
       //  System.out.println(Arrays.deepToString(graph.adjMatrix));
         double time = System.nanoTime();
         graph.startAlgorithm();
@@ -192,18 +182,13 @@ class GraphTest{
         graph.reconstructPath(0,0).foreach(System.out::println);
         time = System.nanoTime()-time;
         System.out.println(time);
-
-
     }
-
     static class Place{
         double x,y;
-
         public Place(double x, double y) {
             this.x = x;
             this.y = y;
         }
-
         public double dist(Place b){
             double difX = this.x-b.x;
             double difY = this.y-b.y;
